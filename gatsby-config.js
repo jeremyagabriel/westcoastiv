5e0ef55cff443b0d6d745be398422228f2f196ea
jeremyagabriel <jeremyagabriel@gmail.com>
2021-02-23 19:03:03 -0800

    google fonts v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,19 +26,23 @@ if (!spaceId || !accessToken) {
 }
 
 module.exports = {
-  /* Your site config here */
   plugins: [
     'gatsby-plugin-theme-ui',
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts-v2`,
       options: {
         fonts: [
           {
-            family: `Roboto`,
-            variants: [`100`, `300`, `400`, `500`, `700`, `900`]
+            family: 'Roboto',
+            weights: ['100', '300', '400', '500', '700', '900']
+          },
+          {
+            family: 'Barlow Condensed',
+            weights: ['100', '300', '400', '500', '600', '700', '900']
           },
         ],
-      },
+        display: 'block',
+      }
     },
     {
       resolve: `gatsby-source-contentful`,
